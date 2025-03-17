f1085bb4ea903f6df82f6216ff0460c035df75ef
Kim Dogyeong <pcs6389@gmail.com>
2025-03-17 17:32:58 +0900

    sql 코드작성

---
+++ Script.sql
@@ -0,0 +1,17 @@
+-- 오라클 관리자
+-- system, sys(최고권한)
+
+-- 사용자이름 : sys as sysdba
+-- 비밀번호 : 엔터
+
+-- 오라클 12c 버전부터 일반적으로 사용자계정 생성시 접두어(c##)를 요구함
+-- c##hr
+-- c## 사용하지 않을때
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+-- 비밀번호 변경
+-- 비밀번호 대소문자 구별함
+ALTER USER hr IDENTIFIED BY hr;
+
+-- 계정 잠금 해제
+-- ALTER USER hr account unlock;
